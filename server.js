b70556937f368d374c588f76e6cd19eee1443fff
AntB <amb.enthusiast@gmail.com>
2013-09-13 15:01:35 -0600

    Added mongodb connection to express server

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+var mongo = require('mongodb');
 var express = require('express');
 
-start(3000);
+var mongoUrl = "mongodb://localhost:27017/observabledb";
+
+mongo.connect(mongoUrl , function(err, db) {
+	
+	if(err) throw err;
+	console.log("Connected to mongoDB @ " + mongoUrl);
+	
+	GLOBAL.dbHandle = db;
+	
+	start(3000);
+});
 
 function start(port) {
 	var app = express();
@@ -14,3 +25,4 @@ function start(port) {
 	app.listen(3000);
 	console.log('Listening on port 3000');
 }
+
