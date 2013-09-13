d15d7fe46163f21fde52a763ff91a579b308a0eb
AntB <amb.enthusiast@gmail.com>
2013-09-13 13:36:58 -0600

    Updated test data

--- tests/queryBuilderTests.js
+++ tests/queryBuilderTests.js
@@ -2,30 +2,28 @@ var assert = require('assert');
 
 var queryBuilder = require('./../queryBuilder');
 
-var time = {
-		start : "",
-		end : ""
-}
 
 describe( 'queryBuilder.buildTimeQuery()' , function() {
 	
 	it('queryBuilder should return valid MongoDB query' , function() {
-		assert.equal("test" , queryBuilder.buildTimeQuery(time) );
+		assert.equal("test" , queryBuilder.buildTimeQuery({ "start" :  "2013-09-12T00:00:00", "end" : "2013-09-12T12:00:00" }) );
 	});
 	
 	it('expected member start not provided', function() {
 		assert.throws(
 			function() {
-				queryBuilder.buildTimeQuery({});
+				queryBuilder.buildTimeQuery({ "end" : "" });
 			},
 			Error
 		);
 	})
 	
+	
+	
 	it('expected member end not provided', function() {
 		assert.throws(
 			function() {
-				queryBuilder.buildTimeQuery({});
+				queryBuilder.buildTimeQuery({ "start" : "" });
 			},
 			Error
 		);
