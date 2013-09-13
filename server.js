5cd51a6e810c56945cc94c756ed919bba2577741
AntB <amb.enthusiast@gmail.com>
2013-09-13 15:21:37 -0600

    Added mongo query to request function

--- server.js
+++ server.js
@@ -17,12 +17,17 @@ function start(port) {
 	var app = express();
 
 	app.use(express.bodyParser());
-
-	app.post('/hello.txt', function(req, res) {
-		res.send(req.body);
-	});
-
+	
+	app.post('/hello.txt', processRequest);
+	
 	app.listen(3000);
 	console.log('Listening on port 3000');
 }
 
+
+function processRequest(req, res) {
+	//res.send(req.body);
+	GLOBAL.dbHandle.collection("A").find( {}).toArray(function(err , docs) {
+		res.send(docs);
+	});
+}
