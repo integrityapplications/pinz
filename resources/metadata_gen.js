9f676a8cc62022bcdb70b7fef6497c95e1c4213f
AntB <amb.enthusiast@gmail.com>
2013-10-08 12:10:45 -0600

    Created a module to create test metadata.  Note use of _id property to record source to enforce uniqueness and auto-indexing.  Just need to remember that to get source, need to call _id property.

---
+++ resources/metadata_gen.js
@@ -0,0 +1,59 @@
+var mongo = require('mongodb');
+// Dont need cmd line args
+
+var sourceA = {
+
+	_id : "A" ,
+
+	desc : "Human readable data definition for source A - what it is, where it comes from, kind of info it contains.",
+
+	attrs : [
+
+		{
+			name : "color" ,
+			type : "string" ,
+			desc : "Property to describe the physical color of the A observable." ,
+			values : [ "red" , "green" , "blue" , "white" , "brown"]
+
+		} ,
+
+		{
+			name : "animal" ,
+			type : "string" ,
+			desc : "The type of animal in the observable." ,
+			values : [ "croc" , "koala" , "kangaroo" , "cockatoo"]
+
+		} ,
+
+		{
+			name : "weight",
+			type : "number",
+			desc : "The mass of the animal in the observable.",
+			low : 0,
+			high : 100,
+			units : "kg"
+		}
+	]
+}
+
+
+var sourceB = {
+
+	_id : "B" ,
+
+	desc : "Human readable data definition for source B - what it is, where it comes from, kind of info it contains.",
+
+	attrs : [
+
+		{
+			name : "level" ,
+			type : "number" ,
+			desc : "Property to describe the level of the observable",
+			low : 0,
+			high : 1000
+
+		}
+	]
+
+};
+
