d8c40e3ae2e899e0adfadc0c482d82bf3e8ce135
Nathan Reese <reese.nathan@gmail.com>
2013-10-08 17:30:14 -0600

    use async

--- examples/earthquakes/earthquakes.js
+++ examples/earthquakes/earthquakes.js
@@ -1,6 +1,8 @@
+var async = require('async');
 var mongo = require('mongodb');
 var request = require('request');
 var xml2js = require('xml2js');
+var usgs =  require('./usgsObservable');
 
 var mongoUrl = "mongodb://localhost:27017/observabledb";
 
@@ -21,20 +23,50 @@ mongo.connect(mongoUrl , function(err, db) {
 });
 
 function pollAtomFeed() {
+	var atomXml = null;
+	var earthquakes = [];
 	console.log("polling server for data...");
-	request('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.atom', function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			xml2js.parseString(body, function (err, result) {
+	async.series([
+		//Make HTTP request
+		function(callback) {
+			request('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.atom', function (error, response, body) {
+				if (response.statusCode !== 200) {
+					callback("Unable to HTTP GET earthquakes, status: " +  response.statusCode + ", error: " + error);
+				} else {
+					atomXml = body;
+					callback();
+				}
+			});
+		},
+		//Parse XML into json and convert to observables
+		function(callback) {
+			xml2js.parseString(atomXml, function (err, result) {
 				if (err) {
-					console.log("Unable to parse response, err: " + err);
+					callback("Unable to parse Atom XML response, err: " + err);
 				} else {
 					//console.log(JSON.stringify(result, null, "\t"));
 					console.log("Found " + result.feed.entry.length + " earthquakes");
+					result.feed.entry.forEach(function (entry, index) {
+						var earthquake = usgs.createQuakeObservable(entry);
+						earthquakes.push(earthquake);
+						//console.log(JSON.stringify(entry, null, "\t"));
+					});
+					callback();
 				}
 			});
-		} else {
-			console.log("Unable to HTTP GET earthquakes, status: " +  response.statusCode + ", error: " + error);
+		},
+		//write earthquake observables to database
+		function(callback) {
+			async.forEach(earthquakes, function(earthquake, insertCallback) {
+				console.log(JSON.stringify(earthquake, null, "\t"));
+				//TODO insert me into database please!
+				insertCallback();
+			}, function(err) {
+				callback();
+			});
 		}
-		console.log("Next polling inverval in 5 minutes. Sit tight");
-	});
+	], function(err) {
+		if (err) console.log(err);
+		console.log("Next polling inverval in 5 minutes. Sit tight...");
+    });
 }
--- examples/earthquakes/usgsObservable.js
+++ examples/earthquakes/usgsObservable.js
@@ -4,24 +4,15 @@ module.exports.createQuakeObservable=createQuakeObservable;
 var parseString = require('xml2js').parseString;
 
 
-function createQuakeObservable(inputXml) {
-	if(inputXml == null) throw new Error("WARN :: Null input XML string!");
-
-	if(inputXml == "") throw new Error("WARN :: Blank input XML string!");
-
-	var resultJson = "";
-	parseString(inputXml, function (err, result) {
-	    resultJson = result;
-	});
-
+function createQuakeObservable(input) {
 	var obs = {};
 
-	obs.id = resultJson.entry.id[0];
+	obs.id = input.id[0];
 	obs.src = "earthquake";
 	obs.attrs = [];
-	obs.t = new Date(resultJson.entry.updated[0]);
+	obs.t = new Date(input.updated[0]);
 
-	var titleValue = resultJson.entry.title[0];
+	var titleValue = input.title[0];
 	var magValue = titleValue.substr(2,3);
 
 
@@ -33,7 +24,7 @@ function createQuakeObservable(inputXml) {
 
 	geoData.id = "Earthquake location";
 
-	var geoPoint = resultJson.entry["georss:point"][0].split(" ");
+	var geoPoint = input["georss:point"][0].split(" ");
 	var pointGeoJson = {
 						"type" : "Point",
 						//georss = lat/lon vs geoJson lon/lat
@@ -44,6 +35,7 @@ function createQuakeObservable(inputXml) {
 
 	obs.geos.push(geoData);
 
+	return obs;
 }
 
 // createQuakeObservable('<entry><id>urn:earthquake-usgs-gov:nc:72083766</id><title>M 0.5 - 2km WSW of Cobb, California</title><updated>2013-10-08T22:00:34.660Z</updated><link rel="alternate" type="text/html" href="http://earthquake.usgs.gov/earthquakes/eventpage/nc72083766"/><summary type="html"><![CDATA[<p class="quicksummary"><a href="http://earthquake.usgs.gov/earthquakes/eventpage/nc72083766#dyfi" class="mmi-III" title="Did You Feel It? maximum reported intensity (2 reports)">DYFI? - <strong class="roman">III</strong></a></p><dl><dt>Time</dt><dd>2013-10-08 21:33:57 UTC</dd><dd>2013-10-08 14:33:57 -07:00 at epicenter</dd><dt>Location</dt><dd>38.810&deg;N 122.753&deg;W</dd><dt>Depth</dt><dd>1.60 km (0.99 mi)</dd></dl>]]></summary><georss:point>38.8097 -122.7528</georss:point><georss:elev>-1600</georss:elev><category label="Age" term="Past Hour"/><category label="Magnitude" term="Magnitude 0"/></entry>');
