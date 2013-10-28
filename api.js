8da24239f9799e81db090a987af1377488d128d6
AntB <amb.enthusiast@gmail.com>
2013-10-28 16:59:11 -0600

    Added observable conversion code and tests.  Need to add tests for server logic on content-type header.

--- api.js
+++ api.js
@@ -18,40 +18,88 @@ function processDataRequest(req, res) {
 		}
 	}
 
-	var results = [];
-	async.forEach(inputs, function(input, callback) {
-		var query = null;
-		try {
-			query = queryBuilder.buildMongoQuery(input);
-		} catch(e) {
-			console.log(JSON.stringify(e, null, ""));
-			callback({status:400, msg:"Bad Request"});
-		}
-		if (query) {
-			GLOBAL.dbHandle.collection(input.src).find(query).limit(5000).toArray(function(err , docs) {
-				if (err) {
-					callback({status:500, msg:"Server Error"});
-				} else {
-					results = results.concat(docs);
-					callback();
-				}
-			});
+	var pinzJsonBoolean = false;
+	if(req.headers["content-type"] == 'pinz-json') {
+		pinzJsonBoolean = true;
+	}
 
-			if ('true' === req.headers['pinz-debug-query']) {
-				GLOBAL.dbHandle.collection(input.src).find(query).limit(5000).explain(function(err , results) {
-					console.log("Query: " + JSON.stringify(query, null, ' '));
-					console.log("Explain: " + JSON.stringify(results, null, ' '));
-				});
+	var results = [];
+
+	// if we asked for pinz, just do the norm
+	if(pinzJsonBoolean == true) {
+		// assume we want "regular" json
+		async.forEach(inputs, function(input, callback) {
+			var query = null;
+			try {
+				query = queryBuilder.buildMongoQuery(input);
+			} catch(e) {
+				console.log(JSON.stringify(e, null, ""));
+				callback({status:400, msg:"Bad Request"});
 			}
-		}
-		
-	}, function(err) {
-		if (err) {
-			res.send(err.status, err.msg);
-		} else {
-			res.send(200, results);
-		}
-	});
+			if (query) {
+				GLOBAL.dbHandle.collection(input.src).find(query).limit(5000).toArray(function(err , docs) {
+					if (err) {
+						callback({status:500, msg:"Server Error"});
+					} else {
+						results = results.concat(docs);
+						callback();
+					}
+				});
+
+				if ('true' === req.headers['pinz-debug-query']) {
+					GLOBAL.dbHandle.collection(input.src).find(query).limit(5000).explain(function(err , results) {
+						console.log("Query: " + JSON.stringify(query, null, ' '));
+						console.log("Explain: " + JSON.stringify(results, null, ' '));
+					});
+				}
+			}
+			
+		}, function(err) {
+			if (err) {
+				res.send(err.status, err.msg);
+			} else {
+				res.send(200, results);
+			}
+		});
+
+	} else {
+		// assume we want "regular" json
+		async.forEach(inputs, function(input, callback) {
+			var query = null;
+			try {
+				query = queryBuilder.buildMongoQuery(input);
+			} catch(e) {
+				console.log(JSON.stringify(e, null, ""));
+				callback({status:400, msg:"Bad Request"});
+			}
+			if (query) {
+				GLOBAL.dbHandle.collection(input.src).find(query).limit(5000).toArray(function(err , docs) {
+					if (err) {
+						callback({status:500, msg:"Server Error"});
+					} else {
+						// modify in place
+						convertPinzObsArrayToRegularJson(docs);
+						results = results.concat(docs);
+						callback();
+					}
+				});
+
+				if ('true' === req.headers['pinz-debug-query']) {
+					GLOBAL.dbHandle.collection(input.src).find(query).limit(5000).explain(function(err , results) {
+						console.log("Query: " + JSON.stringify(query, null, ' '));
+						console.log("Explain: " + JSON.stringify(results, null, ' '));
+					});
+				}
+			}
+			
+		}, function(err) {
+			if (err) {
+				res.send(err.status, err.msg);
+			} else {
+				res.send(200, results);
+			}
+		});
+	}
 }
 
 function processMetadataRequest(req, res) {
@@ -64,5 +112,61 @@ function processMetadataRequest(req, res) {
 	});
 }
 
+
+function convertPinzObsToRegularJson(pinzObs) {
+	// converts a pinz-style obs to a regular-style json obs.
+	if(pinzObs == null) {
+		return null;
+	} else {
+		regularObs = {};
+		// now add obs level data, if present
+		if(pinzObs["id"] != undefined) {
+			regularObs.id = pinzObs["id"];
+		}
+		if(pinzObs["src"] != undefined) {
+			regularObs.src = pinzObs["src"];
+		}
+		if(pinzObs["t"] != undefined) {
+			regularObs.t = pinzObs["t"];
+		}
+		if(pinzObs["attrs"] != undefined) {
+
+			for(var attrIdx = 0; attrIdx < pinzObs.attrs.length; attrIdx++) {		
+				var attr = pinzObs.attrs[attrIdx];
+				valueObs = {};
+
+				if(attr["k"] != undefined && attr["v"] != undefined) {
+					valueObs[attr["k"]] = attr["v"];
+					if(attr["u"] != undefined) {
+						valueObs.u = attr["u"];
+					}
+				}
+				regularObs[attr["k"]] = valueObs;
+			}
+		}
+		if(pinzObs["geos"] != undefined) {
+			regularObs.geos = pinzObs["geos"];
+		}
+		return regularObs;
+	}
+}
+
+function convertPinzObsArrayToRegularJson(pinzObsArray) {
+	// Modifes input array in place.  Expects array of pinz-style observables.
+	// Each obs may have id, src, t, attributes { "k" : "key" , "v" : "value" , "u" : "units"} and geos.
+	if(pinzObsArray == null || pinzObsArray.length == 0) {
+		console.log("\tWARNING: Input pinzJson result was either null or had no observables.  Returning null.");
+		return null;
+	} else {
+
+		pinzObsArray.forEach(function(element, index, array) {
+			pinzObsArray[index] = convertPinzObsToRegularJson(element);
+		});
+	}
+}
+
+
 module.exports.processDataRequest=processDataRequest;
 module.exports.processMetadataRequest=processMetadataRequest;
+module.exports.convertPinzObsToRegularJson=convertPinzObsToRegularJson;
+module.exports.convertPinzObsArrayToRegularJson=convertPinzObsArrayToRegularJson;
--- tests/apiTests.js
+++ tests/apiTests.js
@@ -102,4 +102,159 @@ describe( 'api.processMetadataRequest', function() {
 		assert.equal(3, res.body.length);
 	});
 });
+
+
+describe( 'api.convertPinzObsToRegularJson' , function() {
+
+	it('Null pinz observable gives null result' , function(){
+		assert.equal( null,  api.convertPinzObsToRegularJson(null));
+	});
+
+	it('Invalid obs gives empty object result' , function(){
+		result = api.convertPinzObsToRegularJson({ 'fred' : 'bloggs'});
+		assert.deepEqual({} , result);
+	});
+
+	it('Legitimate pinz-obs gives valid observable JSON' , function(){
+		testDate = new Date(2013,5,13);
+		testPinzObs = {
+	  		id : 'obsId1',
+	  		src : 'obsSource1',
+	  		t : testDate,
+	  		attrs : [
+		    	{
+		   		   k : 'domainAttr_1' ,
+		   		   v : 'someValue1'
+		    	},
+		    	{
+		      		k : 'domainAttr_2',
+		      		v : 1 ,
+		      		u : 'MPH'
+		    	}
+		  	],
+			geos : [
+				{
+	  				id : 'geolocationID_obs1',
+	  				loc : {
+	    					type : 'Point',
+	    					coordinates : [-60, 30]
+	  				}
+				}
+			]
+	};
+		testPrint = '{"id":"obsId1","src":"obsSource1","t":"2013-06-13T06:00:00.000Z","domainAttr_1":{"domainAttr_1":"someValue1"},"domainAttr_2":{"domainAttr_2":1,"u":"MPH"},"geos":[{"id":"geolocationID_obs1","loc":{"type":"Point","coordinates":[-60,30]}}]}';
+		assert.equal(testPrint , JSON.stringify(api.convertPinzObsToRegularJson(testPinzObs)));
+
+	});
+});
+
+
+
+
+
+describe( 'api.convertPinzObsArrayToRegularJson' , function() {
+
+
+	it('Null input returns null' , function() {
+		result = api.convertPinzObsArrayToRegularJson(null);
+		assert.equal(null , result);
+	});
+
+	it('Empty obs array returns null' , function() {
+		result = api.convertPinzObsArrayToRegularJson([]);
+		assert.equal(null , result);
+	});
+
+	it('Valid pinzJson gives correctly formed obs array' , function() {
+		// Seems months are zero-indexed in this constructor!
+		var testDate = new Date(2013,5,13);
+		var testData = [
+			{
+		  		id : 'obsId1',
+		  		src : 'obsSource1',
+		  		t : testDate,
+		  		attrs : [
+				    	{
+				   		   k : 'domainAttr_1' ,
+				   		   v : 'someValue1'
+				    	},
+				    	{
+				      		k : 'domainAttr_2',
+				      		v : 1 ,
+				      		u : 'MPH'
+				    	}
+				  	],
+		  			geos : [
+		    				{
+		      					id : 'geolocationID_obs1',
+		      					loc : {
+		        						type : 'Point',
+		        						coordinates : [-60, 30]
+		      						}
+		    				}
+		  			]
+			} ,
+
+			{
+		  		id : 'obsId2',
+		  		src : 'obsSource1',
+		  		t : testDate,
+		  		attrs : [
+				    	{
+				   		   k : 'domainAttr_1' ,
+				   		   v : 'someOtherValue1'
+				    	},
+				    	{
+				      		k : 'domainAttr_2',
+				      		v : 3 ,
+				      		u : 'MPH'
+				    	}
+				  	],
+		  			geos : [
+		    				{
+		      					id : 'geolocationID_obs2',
+		      					loc : {
+		        						type : 'Point',
+		        						coordinates : [-65, 35]
+		      						}
+		    				}
+		  			]
+			} ,
+
+			{
+		  		id : 'obsId3',
+		  		src : 'obsSource1',
+		  		t : testDate,
+		  		attrs : [
+				    	{
+				   		   k : 'domainAttr_1' ,
+				   		   v : 'yetAnotherValue1'
+				    	},
+				    	{
+				      		k : 'domainAttr_3',
+				      		v : 85 ,
+				      		u : 'Kg'
+				    	}
+				  	],
+		  			geos : [
+		    				{
+		      					id : 'geolocationID_obs3',
+		      					loc : {
+		        						type : 'Point',
+		        						coordinates : [-63, 36]
+		      						}
+		    				}
+		  			]
+			}
+		];
+
+
+		var printResult = '[{"id":"obsId1","src":"obsSource1","t":"2013-06-13T06:00:00.000Z","domainAttr_1":{"domainAttr_1":"someValue1"},"domainAttr_2":{"domainAttr_2":1,"u":"MPH"},"geos":[{"id":"geolocationID_obs1","loc":{"type":"Point","coordinates":[-60,30]}}]},{"id":"obsId2","src":"obsSource1","t":"2013-06-13T06:00:00.000Z","domainAttr_1":{"domainAttr_1":"someOtherValue1"},"domainAttr_2":{"domainAttr_2":3,"u":"MPH"},"geos":[{"id":"geolocationID_obs2","loc":{"type":"Point","coordinates":[-65,35]}}]},{"id":"obsId3","src":"obsSource1","t":"2013-06-13T06:00:00.000Z","domainAttr_1":{"domainAttr_1":"yetAnotherValue1"},"domainAttr_3":{"domainAttr_3":85,"u":"Kg"},"geos":[{"id":"geolocationID_obs3","loc":{"type":"Point","coordinates":[-63,36]}}]}]';
+		// modify data in place
+		api.convertPinzObsArrayToRegularJson(testData);
+
+		assert.deepEqual(printResult , JSON.stringify(testData));
+	});
+
+});
 	
