0d0350c0e719c43ff062cf671c2ee3a298bb994a
nreese <reese.nathan@gmail.com>
2013-10-28 12:09:37 -0600

    Update msgProcessor.js

--- examples/jms/msgProcessor.js
+++ examples/jms/msgProcessor.js
@@ -21,10 +21,11 @@ function publishMsgs() {
 		if (docs[key].length > 0) {
 			console.log("Attempting to publish " + docs[key].length + " docs to " + key);
 			GLOBAL.dbHandle.collection(key).insert(docs[key], function(err, objects) {
-				if(err) console.error("Unable to publish documents to mongo, error: " +  error);
+				if(err) console.error("Unable to publish documents to mongo, error: " +  err);
 			});
 		}
 	});
+	docs = {};
 }
 
 exports.processMsg=processMsg;
