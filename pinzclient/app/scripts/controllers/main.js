3c42d81a72aef6e60e0e3cc74e7dc220f1dca031
Andy Ennamorato <virtualandy@gmail.com>
2013-10-28 15:09:40 -0600

    initial creation based on yeoman/angular

---
+++ pinzclient/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('pinzclientApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
