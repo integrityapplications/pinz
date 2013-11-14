75f42ddd18a177504d2d6bcbd46f8a8dab6edc12
Andy E <virtualandy@gmail.com>
2013-11-13 21:41:15 -0500

    continuing to work on the directive

--- pinzclient/app/scripts/directives/ngLeaflet.js
+++ pinzclient/app/scripts/directives/ngLeaflet.js
@@ -2,9 +2,11 @@
 
 angular.module('pinzclientApp')
   .directive('ngLeaflet', function () {
+  	var map; 
+
   	function initLeaflet() {
 	    var cLat = -37.81, cLon = 144.93;
-	    var map = L.map('map',{  fullscreenControl: true }).setView([cLat, cLon], 11 ); 
+	    map = L.map('map',{  fullscreenControl: true }).setView([cLat, cLon], 11 ); 
 
 	    //L.control.mousePosition().addTo(map);
 	    var tileLayer = 
@@ -15,6 +17,15 @@ angular.module('pinzclientApp')
 	      maxBounds: L.LatLngBounds( L.LatLng(-90,-180), L.LatLng(90,180) ) // sw, ne
 	    });
 	    map.addLayer( tileLayer );
+
+	    var overlays = {};
+	    var markers =[ new L.LayerGroup(),  new L.LayerGroup(), new L.LayerGroup() ];
+	    for ( var i=0; i<markers.length; i++ ){
+	      map.addLayer(markers[i]);
+	      overlays[ "data " + (i+1) ] = markers[i];
+	    }
+	    L.control.layers(null, overlays, {collapsed:false}).addTo( map );
+
 	    //var baseLayers = {
 	    //"CloudMade": tileLayer
 	    //};
@@ -27,11 +38,14 @@ angular.module('pinzclientApp')
     return {
       restrict: 'A',
       scope: {
-              pinz: '='
+              pinz: '=',
+              metadata: '='
       },	
       template: '<div id="map"></div>',
       link: function postLink(scope, element, attrs) {
         //element.text('this is the ngLeaflet directive');
+        console.log('init leaflet with $rootScope metadata ', scope.metadata);
+
         scope.$watch('pinz', function(latestPinz, prevPinz) {
         	//element.text('new name: ', newname, ' was: ', oldname);
          	//updateElement(element, newname);
