3cd9a9c4096b1a336aa1ff768062453b8afaeccc
AntB <amb.enthusiast@gmail.com>
2013-11-25 13:20:41 -0700

    Tidy up of the QueryCtrl removing console logs

--- pinzclient/app/scripts/controllers/QueryCtrl.js
+++ pinzclient/app/scripts/controllers/QueryCtrl.js
@@ -3,9 +3,6 @@
 angular.module('pinzclientApp')
   .controller('QueryCtrl', function($scope, Metadataservice, dataService) {
     
-    console.log("Hello from QueryCtrl");
-    //$scope.dataQuery = {};
-
     Metadataservice.getMetadata(function(dataSources) {
     	$scope.dataSources = dataSources;
     	if($scope.dataQuery === null || typeof $scope.dataQuery === "undefined") {
@@ -13,48 +10,30 @@ angular.module('pinzclientApp')
     	}
 
     });
-
-    $scope.counter = 0;
+    
     $scope.$watch(function() {
     	return $scope.dataQuery }
     	, function(newVal, oldVal) {
-    	console.log('newval ', JSON.stringify(newVal), 'oldval ', JSON.stringify(oldVal));
     	dataService.setQuery(newVal);
-
-  		$scope.counter = $scope.counter + 1;
-  		console.log('counter ', $scope.counter);
-	},
-	true);
-
-	$scope.change = function(attr) {
-		console.log("some attr ", attr);
-		console.log("and data query is now ", $scope.dataQuery);
-	}
-
+	}, true);
+    
 	function createDefaultQuery() {
-
 		var defaultQuery = [];
-	
-    	// now set up initial query object from metadata
     	var sources = $scope.dataSources;
-
-	    console.log(sources.length + " data sources");
 	    var idx;
+
 	    for(idx=0 ; idx < sources.length; ++idx) {
 	    	var source =  sources[idx];
 	        var sourceQuery = {};
 		    sourceQuery.src = source._id;
-		    console.log("Source src = " + sourceQuery.src + " from " + source._id);
 
-	        // blank geos - bind to polygon from geo tab
 	        sourceQuery.geo_within = [];
 
-	        // default time range - bind to UI range sliders
 	        sourceQuery.time_within = {
 	            start : new Date(),
 	            end : new Date()
 	        };
-
+	        
 	        sourceQuery.attrs = [];
 	        var attrsIdx;
 	        for(attrsIdx=0; attrsIdx < source.attrs.length; ++attrsIdx) {
@@ -71,12 +50,11 @@ angular.module('pinzclientApp')
 	        }
 	        defaultQuery.push(sourceQuery);
 	    }
-	    console.log("Query over " + defaultQuery.length + " sources");
-	    console.log(JSON.stringify(defaultQuery));
-	    return defaultQuery;
-	} 
-    
-  });
+
+	    $scope.dataQuery = defaultQuery;
+	    console.log("Data query set to:\n" + JSON.stringify($scope.dataQuery));
+	}
+});
 
 
 
