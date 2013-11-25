5bee9bd6aac99928577557f511cd6edae33d92b7
Andy E <virtualandy@gmail.com>
2013-11-25 11:37:56 -0700

    Map and QueryCtrl now share dataQuery via service. dataQuery is explicitly exported on ds object that is returned as on service init. Map/QueryCtrl now use ds.dataQuery vice getter (though they still use a setter).

--- pinzclient/app/scripts/controllers/Map.js
+++ pinzclient/app/scripts/controllers/Map.js
@@ -5,27 +5,6 @@ angular.module('pinzclientApp')
     $scope.mapversion = 0.1;
     $scope.startingCity = "Melbourne";
 
-    // function initLeaflet() {
-	   //  var cLat = -37.81, cLon = 144.93;
-	   //  var map = L.map('map',{  fullscreenControl: true }).setView([cLat, cLon], 11 ); 
-
-	   //  //L.control.mousePosition().addTo(map);
-	   //  var tileLayer = 
-	   //  L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
-	   //    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-	   //    maxZoom: 18,
-	   //    minZoom: 2
-	   //    //maxBounds: L.LatLngBounds( L.LatLng(-90,-180), L.LatLng(90,180) ) // sw, ne
-	   //  });
-	   //  map.addLayer( tileLayer );
-	   //  //var baseLayers = {
-	   //  //"CloudMade": tileLayer
-	   //  //};
-	
-    // }
-
-    // initLeaflet();
-
     $scope.cancelDataFeed = null;
     $scope.updateCount = 0;
 
@@ -41,40 +20,15 @@ angular.module('pinzclientApp')
     }
 
 	$scope.startDataFeed = function() {
-		// buildQuery(function(query) {
-			console.log('start the data feed with query ', dataService.dataQuery);
-            console.log('is it in parent? ', $scope.$parent.rootDataQuery);
-		// 	updateData(query);
-		// });
-        $scope.dataQuery = dataService.dataQuery; // Need serious thought!
-        updateData($scope.dataQuery);
+		console.log('start the data feed with query ', dataService.dataQuery);
+        $scope.realDataQuery = dataService.dataQuery; // Need serious thought!
+        updateData($scope.realDataQuery);
 	}
 
 	$scope.stopDataFeed = function() {
 		console.log('stop the data feed');
 		$timeout.cancel($scope.cancelDataFeed);
 		$scope.statusMessage = "stopped";
-	}
-
-    // get data from service
-
-    // function buildQuery(callback) {
-    // 	// This is a stub for the real function that handles the returned JSON from the data-api
-    // 	// At the mo, mock it up with something from metadata collection
-    // 	var queryToSend = [];
-
-    // 	Metadataservice.getMetadata(function (data) {
-    // 		// result of hitting /metadata
-    // 		// array of description JSON
-    // 		data.forEach(function(item, index) {
-    // 			var dataSourceOpts = {
-    // 				"src": item._id,
-    // 			}
-    // 			queryToSend.push(dataSourceOpts);
-    // 		});
-    // 		console.log("query is ", queryToSend);
-    // 		callback(queryToSend);
-    // 	});
-    // }
+	}   
     
   });
