d08c9d34ef4c752a471e1d3a932d725f9556b32c
AntB <amb.enthusiast@gmail.com>
2013-11-27 21:46:02 -0700

    Some shonky dynamic pillboxes, courtesy of keeping state varaible.  Added additional validation in dataService to make sure the query that is dispatchd to the API is free from unnecessary clutter.  With some refactoring, may want to incorporate into the proper pinz client

--- pinzclient/app/scripts/services/dataService.js
+++ pinzclient/app/scripts/services/dataService.js
@@ -8,6 +8,75 @@ angular.module('modalApp')
     var query = {};
     var ds = {};
 
+
+    // util function
+    var clearQuery = function(queryIn) {
+
+		var cleanQuery = [];
+
+		if(queryIn != null && typeof query != 'undefined') {
+
+    		if((queryIn instanceof Array) == true) {
+
+    			if(queryIn.length == 0) {
+
+    				var numSrcQueries = queryIn.length;
+    				var srcIdx;
+
+    				for(srcIdx = 0 ; srcIdx < numSrcQueries ; srcIdx++) {
+    					var tempSrcQuery = {};
+
+    					var srcQuery = queryIn[srcIdx];
+    					srcQuery.src = queryIn[srcIdx].src;
+    					if("geo_within" in srcQuery) {
+    						if(srcQuery.geo_within instanceof Array) {
+    							if(srcQuery.geo_within.length > 0) {
+    								tempSrcQuery.geo_within = srcQuery.geo_within;
+    							}
+    						}
+    					}
+
+    					if("time_within" in srcQuery) {
+    						if(("start" in srcQuery.time_within) && ("end" in srcQuery.time_within)  ) {
+    							tempSrcQuery.time_within = srcQuery.time_within;
+    						}
+    					}
+
+    					if("attrs" in srcQuery) {
+    						if(srcQuery.attrs instanceof Array) {
+    							if(srcQuery.attrs.length > 0) {
+    								var tempAttrs = [];
+    								var attrIdx;
+
+    								for(attrIdx = 0; attrIdx < srcQuery.attrs.length; attrIdx++) {
+    									var attibute = srcQuery.attrs[attrIdx];
+    									if("k" in attribute && ("v" in attribute || "high" in attribute || "low" in attribute )) {
+    										tempAttrs.push(attribute);
+    									}
+    								}
+    								tempSrcQuery.attrs = tempAttrs;
+    							}
+    						}
+
+    					}
+    					cleanQuery.push(tempSrcQuery);
+    					console.log("Updated cleanQuery: " + JSON.stringify(cleanQuery));
+    				}
+
+    			} else {
+    				console.log("WARNING: Input query array has 0 entries!");
+    			}
+    		}
+    	}
+    	console.log("The *actual* query sent to the server is...\n" + JSON.stringify(cleanQuery));
+
+	}
+
+
+
+
+
+
     ds.getData = function(query) {
 		//$log.log('Retrieving data with this ', JSON.stringify(query));
 
@@ -39,7 +108,11 @@ angular.module('modalApp')
 	}
 
 	ds.setQuery = function(query) {
+		// place a final check fo null, empy or blank query values
+		// ds.dataQuery = cleanDataQuery(query);
+	    
 		ds.dataQuery = query;
+
 	}
 
     return ds;
