65c1199019edd2bd05722301a98d7c518e93179e
Andy E <virtualandy@gmail.com>
2013-12-20 17:07:56 -0700

    taking a crack and being able to draw custom shapes for use in query

--- pinzclient/app/scripts/controllers/GeoInputController.js
+++ pinzclient/app/scripts/controllers/GeoInputController.js
@@ -2,12 +2,27 @@
 
 angular.module('modalApp')
   .controller('GeoInputController', function ($scope) {
-    //$scope.latitude="33.33";
-    //$scope.longitude="130.44";
-
-    console.log('if i\'m not in the page...');
+    $scope.ulLatitude="33.33";
+    $scope.ulLongitude="130.44";
+    $scope.lrLatitude="30.33";
+    $scope.lrLongitude="131.44";
+    $scope.customShape={};
+    $scope.bbox = {};
+    $scope.bbox.ulError = false;
+    $scope.bbox.lrError = false;
+    $scope.bbox.ulErrorMessage = "";
+    $scope.bbox.lrErrorMessage = "";
+    $scope.bbox.initialized = false;
 
     $scope.printCtrlLatLon = function() {
-    	console.log("lat lon ", $scope.latitude, $scope.longitude);
+    	if ($scope.bbox.ulLatitude || $scope.bbox.initalized) {
+    		console.log("bbox: ", $scope.bbox);
+    	} else {
+    		console.log("custom shape: ", $scope.customShape);
+    	}
     }
+
+    /*$scope.$watch("customShape", function(newShape, oldShape) {
+    	console.log("GeoCtrl $watch - shape they just drew: ", newShape);
+    })*/
   });
---
+++ pinzclient/app/scripts/directives/pinzLeafletDraw.js
@@ -0,0 +1,103 @@
+'use strict';
+
+angular.module('modalApp')
+  .directive('pinzLeafletDraw', function () {
+
+  	function initLeafletDraw(setCustomShape) {
+
+      var drawnItems = new L.FeatureGroup();
+  	  // create a map in the "map" div, set the view to a given place and zoom
+      var options = {
+          position: 'topleft',
+          draw: {
+              polyline: false,
+              polygon: {
+                  allowIntersection: false, // Restricts shapes to simple polygons
+                  drawError: {
+                      color: '#e1e100', // Color the shape will turn when intersects
+                      message: '<strong>Oh snap!<strong> you can\'t draw that!' // Message that will show when intersect
+                  },
+                  shapeOptions: {
+                      color: '#377eb8'
+                  }
+              },
+              circle: false, // Turns off this drawing tool
+              rectangle: {
+                  shapeOptions: {
+                      color: '#377eb8',
+                      weight: 10,
+                  }
+              },
+              marker: false
+          },
+          edit: {
+              featureGroup: drawnItems, //REQUIRED!!
+              remove: true,
+              edit: true
+          }
+      };
+
+      var mapDraw = L.map('mapdraw', {"center": [51.505, -0.09], "zoom": 10});
+
+      var drawControl = new L.Control.Draw(options);
+      mapDraw.addControl(drawControl);  	
+      
+      
+      mapDraw.addLayer(drawnItems);
+  		// add an OpenStreetMap tile layer
+  		//L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  	  //  	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  		//}).addTo(mapDraw);
+      L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+        maxZoom: 18,
+        minZoom: 1,
+        maxBounds: L.LatLngBounds( L.LatLng(-90,-180), L.LatLng(90,180) ) // sw, ne
+      }).addTo(mapDraw);
+
+      mapDraw.on('draw:created', function (e) {
+          var type = e.layerType,
+              layer = e.layer; 
+              /* layer is a Leaflet shape
+               * i.e. http://leafletjs.com/reference.html#polygon
+               */
+
+          console.log("layer geojson ", layer.toGeoJSON());
+          if (type === 'marker') {
+              layer.bindPopup('A popup!');
+          } else {
+            layer.bindPopup(type + " popup");
+          }
+
+          setCustomShape(layer.toGeoJSON());
+          drawnItems.addLayer(layer);
+      });
+      console.log('finished initing leaflet draw');
+
+      $('#myModal').on('show.bs.modal', function(){
+        console.log('redraw the map?');
+        setTimeout(function() {
+          mapDraw.invalidateSize();
+        }, 2000);
+       });
+  	}
+
+    return {
+      template: '<div id="mapdraw" style="height: 300px;"></div>',
+      restrict: 'A',
+      scope: {
+        customGeo : "="
+      },
+      link: function ($scope, element, attrs) {
+        function setCustomShape(geoJsonShape) {
+          $scope.$apply(function() {
+            console.log("what was custom shape? ", $scope.customGeo);
+            $scope.customGeo = geoJsonShape;
+          })
+        }
+        console.log('do the map draw thing', $scope.customShape);
+        //element.css({"height": "300px"})
+        initLeafletDraw(setCustomShape);
+      }
+    }
+  });
